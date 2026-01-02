2a4f2f014f22032fe91087be316e9c9b548874ba
ASHISH PATHAK <143995643+ASura12@users.noreply.github.com>
2026-01-02 20:41:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (70.65%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.cpp
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int secondHighest(string s) {
+        /*
         int highest = -1;
         int secondhighest = -1;
         vector<int>nums;
@@ -18,5 +19,36 @@ public:
             }
         }
         return secondhighest;
+        */
+        int highest = -1;
+        int secondhighest = -1;
+        for(char c : s){
+            if(c >= '0' && c <= '9'){
+                int d = c - '0';
+                if(d > highest){
+                    secondhighest = highest;
+                    highest = d; 
+                }else if(d < highest && d > secondhighest){
+                    secondhighest = d;
+                }
+            }
+        }
+        return secondhighest;
     }
 };
+/*
+int highest = -1;
+int secondhighest = -1;
+for(char c : s){
+    if(c >= '0' && c <= '9'){
+        int d = c - '0';
+        if(d > highest){
+            secondhighest = highest;
+            highest = d; 
+        }else if(d < highest && d > secondhighest){
+            secondhighest = d;
+        }
+    }
+}
+return secondhighest;
+*/
