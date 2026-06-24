3ff7dd8fb5700913efcab82309ca0529ffc160ed
ASHISH PATHAK <143995643+ASura12@users.noreply.github.com>
2026-06-24 17:11:54 +0530

    Time: 3 ms (29.02%) | Memory: 10.4 MB (12.54%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string filtered= "";
+        for(char ch : s){
+            if(isalnum(ch)){
+                filtered += tolower(ch);
+            }
+        }
+        int left = 0,right = filtered.size()-1;
+        while(left<right){
+            if(filtered[left] != filtered[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
