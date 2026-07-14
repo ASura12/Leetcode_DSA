5bbedef5487ad073bdac36f69910050185e02c85
ASHISH PATHAK <143995643+ASura12@users.noreply.github.com>
2026-07-14 20:31:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (54.27%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    int firstOcc(vector<int> &arr,int key,bool isSearchingLeft){
-        int s = 0;
-        int e = arr.size()-1;
-        int mid = s + (e-s)/2;
+
+    int search(vector<int>&nums,int target,bool isleft){
+        int start = 0,end = nums.size()-1;
         int ans = -1;
-        while(e >= s){
-            if(key > arr[mid]){
-                s = mid + 1;
-            }else if(key < arr[mid]){
-                e = mid -1;
+        while(start<=end){
+            int mid = start + (end- start)/2;
+            if(nums[mid] > target){
+                end = mid -1;
+            }else if(nums[mid] < target){
+                start = mid + 1;
             }else{
                 ans = mid;
-                if(isSearchingLeft){
-                    e = mid - 1;
+                if(isleft){
+                    end = mid-1;
                 }else{
-                    s = mid + 1;
+                    start = mid + 1;
                 }
             }
-            mid = s + (e-s)/2;
         }
         return ans;
     }
-    
+
     vector<int> searchRange(vector<int>& nums, int target) {
-    
-        vector<int> result = {-1, -1};
-        int FirstOcc = firstOcc(nums,target,true);
-        int LastOcc = firstOcc(nums,target,false);
-        result[0] = FirstOcc;
-        result[1] = LastOcc;
-        return result;    
+        vector<int> result = {-1,-1};
+        int First = search(nums,target,true);
+        int last = search(nums,target,false);
+        result[0] = First;
+        result[1] = last;
+        return result;
     }
 };
