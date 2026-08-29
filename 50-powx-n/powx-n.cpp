f57212c3e91e67d507cc2a5173f26a5d509946a9
ASHISH PATHAK <143995643+ASura12@users.noreply.github.com>
2026-08-29 17:05:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.61%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+    double solve(double x,long long n){
+        if(n == 0) return 1.0;
+        double half = solve(x, n/2);
+        if(n % 2 == 0){
+            return half * half;
+        }else{
+            return x * half * half;
+        }
+    }
+
+    double myPow(double x, int n) {
+        long long exp = n;
+        if(n < 0){
+            return 1.0/ solve(x, -exp);
+        }
+        return solve(x,exp);
+    }
+};
